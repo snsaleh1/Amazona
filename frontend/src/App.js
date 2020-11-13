e3f3305cfbda8b91a848940b05aa4079b863412b
snsaleh1 <snsaleh11@gmail.com>
2020-11-13 16:50:47 -0600

    resize product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import data from './data';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -42,33 +45,7 @@ function App() {
         <div className="content">
           <Route path="/products/:id" component={ProductScreen} />
           <Route path="/" exact={true} component={HomeScreen} />
-          <ul className="products">
-            {
-              data.products.map(product => 
-                <li>
-              <div className="product">
-                <img className="product-image" src={product.image} alt="product"></img>
-                  <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">{product.price}</div>
-                  <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
-                    </div> 
-                </li>)
-            }
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="product"></img>
-                    <div className="product-name">
-                      <a href="product.html">FLIGHTLESS By Caleb Saleh</a>
-                    </div>
-                    <div className="product-brand">Children's Picture Book</div>
-                    <div className="product-price">$15</div>
-                    <div className="product-rating">5.0 Stars (198 reviews)</div>
-                    </div> 
-                </li>
-            </ul>
+          
         </div>
     </main>
           <footer className="footer">
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import data from '../data';
 
 function HomeScreen (props) {
-    return <div>HomeScreen</div>
+    return <ul className="products">
+    {
+      data.products.map(product => 
+        <li>
+      <div className="product">
+        <img className="product-image" src={product.image} alt="product"></img>
+          <div className="product-name">
+            <a href="product.html">{product.name}</a>
+          </div>
+          <div className="product-brand">{product.brand}</div>
+          <div className="product-price">{product.price}</div>
+          <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
+            </div> 
+        </li>)
+    }
+      <li>
+        <div className="product">
+          <img className="product-image" src="images/d1.jpg" alt="product"></img>
+            <div className="product-name">
+              <a href="product.html">FLIGHTLESS By Caleb Saleh</a>
+            </div>
+            <div className="product-brand">Children's Picture Book</div>
+            <div className="product-price">$15</div>
+            <div className="product-rating">5.0 Stars (198 reviews)</div>
+            </div> 
+        </li>
+    </ul>
 }
 export default HomeScreen;
